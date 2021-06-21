ca84b0f3786ff74f00097382ff7ce8f45b706e82
LightningYTlol <73615108+LightningYTlol@users.noreply.github.com>
2021-06-20 21:38:39 -0700

    Update itemevents.java

--- TerminatorTM/src/me/lightning/terminator/events/itemevents.java
+++ TerminatorTM/src/me/lightning/terminator/events/itemevents.java
@@ -7,8 +7,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import me.lightning.terminator.items.itemManager;
+import org.bukkit.util.Vector;
 
 import static org.bukkit.Sound.ENTITY_ENDERMAN_TELEPORT;
 
@@ -16,7 +18,7 @@ public class itemevents implements Listener {
 
     @EventHandler
     public static void onLeftClick(PlayerInteractEvent event) {
-        if (event.getAction() == Action.LEFT_CLICK_AIR) {
+        if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
             if (event.getItem() != null) {
                 if (event.getItem().getItemMeta().equals(itemManager.Terminator.getItemMeta())) {
                     Player player = event.getPlayer();
@@ -24,6 +26,14 @@ public class itemevents implements Listener {
                     player.launchProjectile(Arrow.class);
                     player.launchProjectile(Arrow.class);
                 }
+            }
+        }
+    }
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Arrow) {
+            Arrow arrow = (Arrow)e.getDamager();
+            if (arrow.getShooter() instanceof Player) {
+                e.setDamage(100);
             }
         }
     }
